a4410fb441befd445b3b2901dccf445cbde3708a
Emrin <emrin95@gmail.com>
2023-08-18 22:00:36 +0200

    feat(app): add domain name

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
     unoptimized: true,
   }
 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
-
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
 
